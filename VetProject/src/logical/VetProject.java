1c545a6af93d3ca0318baee5bfc7cc335b439141
Diego17 <diegoalbertocastropastor@gmail.com>
2024-08-27 11:45:48 -0500

    Crear interfaz base

--- VetProject/src/logical/VetProject.java
+++ VetProject/src/logical/VetProject.java
@@ -1,7 +1,12 @@
 package logical;
+
+import gui.InicioVet;
+
 public class VetProject {
     public static void main(String[] args) {
-        
+        InicioVet vet = new InicioVet();
+        vet.setVisible(true);
+        vet.setLocationRelativeTo(null);
     }
     
 }
