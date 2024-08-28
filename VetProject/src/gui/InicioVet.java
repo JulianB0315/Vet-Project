f3a35ebc748403aefde2913f5c98d174c8d35ff2
Diego17 <diegoalbertocastropastor@gmail.com>
2024-08-28 10:35:33 -0500

    Form agregado

--- VetProject/src/gui/InicioVet.java
+++ VetProject/src/gui/InicioVet.java
@@ -1,4 +1,7 @@
 package gui;
+
+import java.awt.Color;
+
 public class InicioVet extends javax.swing.JFrame {
     public InicioVet() {
         initComponents();
@@ -29,12 +32,34 @@ public class InicioVet extends javax.swing.JFrame {
         labelBanner.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/Default_Create_a_vibrant_banner_featuring_a_diverse_assembly_o_3.jpg"))); // NOI18N
         panelInicioVet.add(labelBanner, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 890, 270));
 
-        btnRegistro.setBackground(new java.awt.Color(86, 168, 124));
+        btnRegistro.setBackground(new java.awt.Color(28, 155, 128));
         btnRegistro.setFont(new java.awt.Font("Lucida Sans", 1, 24)); // NOI18N
         btnRegistro.setForeground(new java.awt.Color(217, 227, 234));
         btnRegistro.setText("Registrar Consulta");
         btnRegistro.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        panelInicioVet.add(btnRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 440, 270, 80));
+        btnRegistro.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnRegistroMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnRegistroMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnRegistroMouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                btnRegistroMousePressed(evt);
+            }
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                btnRegistroMouseReleased(evt);
+            }
+        });
+        btnRegistro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistroActionPerformed(evt);
+            }
+        });
+        panelInicioVet.add(btnRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 450, 270, 80));
 
         labelSlogan.setFont(new java.awt.Font("Calibri Light", 1, 28)); // NOI18N
         labelSlogan.setForeground(new java.awt.Color(0, 102, 102));
@@ -55,6 +80,33 @@ public class InicioVet extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnRegistroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistroActionPerformed
+        RegistroConsulta register = new RegistroConsulta();
+        register.setVisible(true);
+        register.setLocationRelativeTo(null);
+        this.dispose();
+    }//GEN-LAST:event_btnRegistroActionPerformed
+
+    private void btnRegistroMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnRegistroMouseEntered
+        btnRegistro.setBackground(new Color(86, 168, 124));
+    }//GEN-LAST:event_btnRegistroMouseEntered
+
+    private void btnRegistroMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnRegistroMouseExited
+        btnRegistro.setBackground(new Color(28,155,128));
+    }//GEN-LAST:event_btnRegistroMouseExited
+
+    private void btnRegistroMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnRegistroMouseClicked
+        
+    }//GEN-LAST:event_btnRegistroMouseClicked
+
+    private void btnRegistroMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnRegistroMousePressed
+        
+    }//GEN-LAST:event_btnRegistroMousePressed
+
+    private void btnRegistroMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnRegistroMouseReleased
+       
+    }//GEN-LAST:event_btnRegistroMouseReleased
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
